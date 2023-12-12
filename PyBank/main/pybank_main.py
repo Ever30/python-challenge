9de5ecfb60497188156ba9e5d5865363ee200971
Ever30 <149534473+Ever30@users.noreply.github.com>
2023-12-12 01:06:16 -0600

    Update pybank_main.py

--- PyBank/main/pybank_main.py
+++ PyBank/main/pybank_main.py
@@ -4,14 +4,15 @@ import csv
 budget_data_csv = os.path.join("..", "Resources", "budget_data.csv")
 output_file = os.path.join("..", "analysis", "results.txt")
 
-with open(output_file, "w") as file:    
+with open(output_file, "w") as file:  
+      
     file.write("Financial Analysis\n")
     file.write("-" * 20 + "\n")
 
     # Declaring variables
-    total_months = 0
-    total_profit_loss = 0
-    previous_profit_loss = 0
+    months = 0
+    total_loss = 0
+    previous_loss = 0
     profit_loss_changes = []
     dates = []   
     max_increase = 0
@@ -19,7 +20,7 @@ with open(output_file, "w") as file:
     max_increase_date = ""
     max_decrease_date = ""
 
-    # Opening the csv and read it 
+    # starting the csv  
     with open(budget_data_csv) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
     
@@ -29,20 +30,20 @@ with open(output_file, "w") as file:
      
         for row in csv_reader:
          
-            total_months = total_months + 1  
+            months = months + 1  
 
-            # get profit/loss from current row
             current_profit_loss = int(row[1])  
             dates.append(row[0])  
 
-            #total profit/loss
-            total_profit_loss += current_profit_loss
+            #total 
+            total_loss = total_loss + current_profit_loss
 
-            # calculating changes from previous month
-            if total_months > 1:
-                profit_loss_change = current_profit_loss - previous_profit_loss
+            # opening conditional statement
+            if months > 1 :
+                profit_loss_change = current_profit_loss - previous_loss
                 profit_loss_changes.append(profit_loss_change)
 
+                
                 if profit_loss_change > max_increase:
                     max_increase = profit_loss_change
                
@@ -51,15 +52,17 @@ with open(output_file, "w") as file:
 
                 if profit_loss_change < max_decrease:
                     max_decrease = profit_loss_change
-                    max_decrease_date = row[0]   
+                    max_decrease_date = row[0]  
 
-            previous_profit_loss = current_profit_loss
+             
+            previous_loss = current_profit_loss
 
-    # Calculate the average change
+    # Calculating the average change
     average_change = (round(sum(profit_loss_changes) / len(profit_loss_changes),2))
-
-    file.write(f"Total Months: {total_months}\n")
-    file.write(f"Total: ${total_profit_loss}\n")
+    
+    
+    file.write(f"Total Months: {months}\n")
+    file.write(f"Total: ${total_loss}\n")
     file.write(f"Average Change: $ {(round(average_change,2))}\n")
     file.write(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})\n")
     file.write(f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})\n")
@@ -69,9 +72,9 @@ print("Financial Analysis")
 print("")
 print("___________________________")
 print("")
-print(f"Total Months: {total_months}")
+print(f"Total Months: {months}")
 print("")
-print(f"Total: ${total_profit_loss}")
+print(f"Total: ${total_loss}")
 print("")
 print(f"Average Change: ${(round(average_change,2))}")
 print("")
