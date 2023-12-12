e8b8122c31f4079809ed20aa0defcd7ffcdd4371
Ever30 <149534473+Ever30@users.noreply.github.com>
2023-12-12 01:18:50 -0600

    Update pypoll_main.py

--- PyPoll/main/pypoll_main.py
+++ PyPoll/main/pypoll_main.py
@@ -1,90 +1,70 @@
 import os
 import csv
 
-budget_data_csv = os.path.join("..", "Resources", "budget_data.csv")
-output_file = os.path.join("..", "analysis", "results.txt")
+election_data_csv = os.path.join("..", "Resources", "election_data.csv")
+output_file = os.path.join("..", "analysis", "election_results.txt")
 
-with open(output_file, "w") as file:  
-      
-    file.write("Financial Analysis\n")
-    file.write("-" * 20 + "\n")
+with open(output_file, "w") as f:
+    f.write("Election Results\n")
+    f.write("-" * 20 + "\n")
 
-    # Declaring variables
-    months = 0
-    total_loss = 0
-    previous_loss = 0
-    profit_loss_changes = []
-    dates = []   
-    max_increase = 0
-    max_decrease = 0
-    max_increase_date = ""
-    max_decrease_date = ""
+    # declaring variables
+    total_votes = 0
+    candidate_votes = {}   
+    winner = []   
+    max_votes = 0   
 
-    # starting the csv  
-    with open(budget_data_csv) as csv_file:
+    # starting the csv
+    with open(election_data_csv) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
-    
+
         #code to remove the header
         csv_header = next(csv_reader)
 
-     
         for row in csv_reader:
-         
-            months = months + 1  
+            total_votes = total_votes + 1
+            candidate_name = row[2]   
 
-            current_profit_loss = int(row[1])  
-            dates.append(row[0])  
+            # counting candidate votes with conditional statement 
+            if candidate_name in candidate_votes:
+                candidate_votes[candidate_name] += 1
+            else:
+                candidate_votes[candidate_name] = 1
 
-            #total 
-            total_loss = total_loss + current_profit_loss
+            if candidate_votes[candidate_name] > max_votes:
+                max_votes = candidate_votes[candidate_name]
+                winner = candidate_name
 
-            # opening conditional statement
-            if months > 1 :
-                profit_loss_change = current_profit_loss - previous_loss
-                profit_loss_changes.append(profit_loss_change)
-
-                
-                if profit_loss_change > max_increase:
-                    max_increase = profit_loss_change
-               
-                    #code to get the increase date
-                    max_increase_date = row[0]   
-
-                if profit_loss_change < max_decrease:
-                    max_decrease = profit_loss_change
-                    max_decrease_date = row[0]  
-
-             
-            previous_loss = current_profit_loss
-
-    # Calculating the average change
-    average_change = (round(sum(profit_loss_changes) / len(profit_loss_changes),2))
+    # exporting the results to outside the code
+    f.write(f"Total Votes: {total_votes}\n")
+    f.write("-" * 20 + "\n")
+    for candidate, votes in candidate_votes.items():
+        percentage = (votes / total_votes) * 100
+        f.write(f"{candidate}: {(round(percentage,3))}% ({votes})\n")
     
-    
-    file.write(f"Total Months: {months}\n")
-    file.write(f"Total: ${total_loss}\n")
-    file.write(f"Average Change: $ {(round(average_change,2))}\n")
-    file.write(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})\n")
-    file.write(f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})\n")
+    f.write("-" * 20 + "\n")
+    f.write(f"Winner: {winner}\n")
+    f.write("-" * 20 + "\n")
 
-print ("")
-print("Financial Analysis")
 print("")
-print("___________________________")
+print("Election Results")
 print("")
-print(f"Total Months: {months}")
+print("------------------------------")
 print("")
-print(f"Total: ${total_loss}")
+print(f"Total Votes: {total_votes}")
 print("")
-print(f"Average Change: ${(round(average_change,2))}")
+print("------------------------------")
 print("")
-print(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})")
+for candidate, votes in candidate_votes.items():
+    percentage = (votes / total_votes) * 100
+    print(f"{candidate}: {(round(percentage,3))}% ({votes})")
 print("")
-print(f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})")
+print("------------------------------")
 print("")
-print("Your file was successfully created!")
+print(f"Winner: {winner}")
 print("")
+print("------------------------------")
+print("")
+print("Your file was created!")
 
 
-
-        
